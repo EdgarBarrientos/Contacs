2ce736ee3b60d7fbb0873a2e9b444231a5e8e20f
adrianeto <77219420+adrianeto@users.noreply.github.com>
2021-06-12 13:18:00 -0500

    Adding second test case for API part

---
+++ src/main/java/com/cognizant/ContactList/com/cognizant/ContactList/api/ContactAPI.java
@@ -0,0 +1,23 @@
+package com.cognizant.ContactList.com.cognizant.ContactList.api;
+
+import com.cognizant.ContactList.Domains.ContactList;
+import com.cognizant.ContactList.Services.ContactListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ContactAPI {
+
+    @Autowired
+    private ContactListService service;
+
+    @GetMapping("/Contacts")
+    public ResponseEntity<Iterable<ContactList>> getContacts(){
+        return new ResponseEntity(service.findAllContacts(), HttpStatus.OK);
+    }
+}
--- src/test/java/com/cognizant/ContactList/ContactListApiTest.java
+++ src/test/java/com/cognizant/ContactList/ContactListApiTest.java
@@ -1,14 +1,51 @@
 package com.cognizant.ContactList;
 
+import com.cognizant.ContactList.Domains.ContactList;
+import com.cognizant.ContactList.Services.ContactListService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @AutoConfigureMockMvc
+@WebMvcTest
 public class ContactListApiTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ContactListService service;
+
+    @Test
+    public void getContactsTest() throws Exception{
+        String contactList = getJSON("/ContactList.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<ContactList> listOfContacts = new ObjectMapper().
+                readValue(contactList,new TypeReference<List<ContactList>>(){});
+        when(service.findAllContacts()).thenReturn(listOfContacts);
+
+        mockMvc.perform(get("/Contacts").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        .andExpect( jsonPath("length()" ).value(listOfContacts.size()) );
+
+    }
+
+    private String getJSON(String path) throws Exception {
+        return new String(this.getClass().getResourceAsStream(path).readAllBytes());
+    }
 
 }
